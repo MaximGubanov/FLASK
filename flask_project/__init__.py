13d6cc36069c9ae7a09e8562006b9f6a24817ccc
MaximGubanov <m_gubanov@bk.ru>
2022-06-20 14:56:15 +0300

    lesson_1

---
+++ flask_project/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_project.config import Config
+
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+
+def create_app():
+
+    app = Flask(__name__)
+    db.init_app(app)
+
+    from flask_project.main.routes import main
+    app.register_blueprint(main)
+
+    app.config.from_object(Config)
+    login_manager.init_app(app)
+
+    return app
