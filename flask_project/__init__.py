cf6a10ecd8357b330c5ef3b98eef2cef34bbabf9
MaximGubanov <m_gubanov@bk.ru>
2022-06-21 19:51:09 +0300

    lesson_2

--- flask_project/__init__.py
+++ flask_project/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_project.config import Config
+from flask_bcrypt import Bcrypt
 
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+bcrypt = Bcrypt()
 
 
 def create_app():
@@ -18,5 +20,9 @@ def create_app():
 
     app.config.from_object(Config)
     login_manager.init_app(app)
+    bcrypt = Bcrypt()
+
+    from flask_project.users.routes import users
+    app.register_blueprint(users)
 
     return app
